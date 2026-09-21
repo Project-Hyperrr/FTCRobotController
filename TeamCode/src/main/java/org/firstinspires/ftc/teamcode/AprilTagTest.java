ce43093adc6d652978aa1391c3e0a17d7a5ee12b
Markice Gaskins <markicegaskins7711@gmail.com>
2026-09-21 12:46:43 -0400

    Finished AprilTagTest
    The camera now reads april tags and rotates "rightServo" to them.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagTest.java
@@ -32,6 +32,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -61,28 +64,44 @@ import java.util.List;
  *
  * To experiment with using AprilTags to navigate, try out these two driving samples:
  * RobotAutoDriveToAprilTagOmni and RobotAutoDriveToAprilTagTank
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag Easy", group = "Concept")
-@Disabled
 public class AprilTagTest extends LinearOpMode {
 
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    // Hardware \\
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+    private Servo rightServo = null;
 
-    /**
-     * The variable to store our instance of the AprilTag processor.
-     */
-    private AprilTagProcessor aprilTag;
+    // AprilTag Variables \\
+    private static final boolean USE_WEBCAM = true;  // We're using a webcam so this is true.
 
-    /**
-     * The variable to store our instance of the vision portal.
-     */
-    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag; // Our instance of the AprilTag processor.
+    private VisionPortal visionPortal; // Our instance of the vision portal.
 
+    private int aprilTagTargetID = -1; // The tag to target, or -1 for any.
+
+    // Detection Variables ||
+    private boolean targetFound  = false;
+    private String targetName    = "none";
+    private int    targetID      = 0;
+    private double targetRange   = 0;
+    private double targetBearing = 0;
+    private double targetYaw     = 0;
+
+    // OpMode \\
     @Override
     public void runOpMode() {
+
+        // Get the configured hardware
+        leftMotor  = hardwareMap.get(DcMotor.class, "LeftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "RightMotor");
+
+        rightServo = hardwareMap.get(Servo.class, "rightServo");
+
+        // Flip the motors on one side so they all move the same direction
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
 
         // See ConceptAprilTag.java for how to add a camera compatibility quirk
         initAprilTag();
@@ -91,30 +110,122 @@ public class AprilTagTest extends LinearOpMode {
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
         telemetry.addData(">", "Touch START to start OpMode");
         telemetry.update();
-        waitForStart();
+
+        waitForStart(); // Wait until the OpMode is started.
 
         while (opModeIsActive()) {
+            // Reset the found AprilTag
+            targetFound = false;
 
+            /* This code is commented as it currently isn't in use.
             telemetryAprilTag();
-
-            // Push telemetry to the Driver Station.
             telemetry.update();
+            */
 
-            // Save CPU resources; can resume streaming when needed.
-            if (gamepad1.dpad_down) {
+
+            // Gamepad Controls \\
+
+            if (gamepad1.dpad_down) { // When dpad down is pressed, turn off streaming, save resources
                 visionPortal.stopStreaming();
-            } else if (gamepad1.dpad_up) {
+            } else if (gamepad1.dpad_up) { // When dpad up is pressed, turn on streaming
                 visionPortal.resumeStreaming();
             }
 
-            // Share the CPU.
+            if (gamepad1.dpad_left) {
+                // When dpad left is pressed, decrease AprilTag target by 1
+                aprilTagTargetID -= 1;
+
+                if (aprilTagTargetID < 0) {
+                    telemetry.addData("Target April Tag", aprilTagTargetID + " = Any AprilTag");
+                } else {
+                    telemetry.addData("Target April Tag", aprilTagTargetID);
+                }
+                telemetry.update();
+            } else if (gamepad1.dpad_right) {
+                // When dpad right is pressed, increase AprilTag target by 1
+                aprilTagTargetID += 1;
+
+                if (aprilTagTargetID < 0) {
+                    telemetry.addData("Target April Tag", "Any AprilTag");
+                } else {
+                    telemetry.addData("Target April Tag", aprilTagTargetID);
+                }
+                telemetry.update();
+            }
+
+
+            // AprilTag Detections \\
+
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections(); // Get all detected april tags
+
+            for (AprilTagDetection detection : currentDetections) { // For each detection in the detected april tags...
+
+                // If the detection is a Single AprilTag...
+                if (detection instanceof AprilTagSingleDetection) {
+
+                    // Change the detection type from an `AprilTagDetection` to a `AprilTagSingleDetection`
+                    AprilTagSingleDetection singleDetection = (AprilTagSingleDetection) detection;
+
+                    // Check if there is information on this tag in SDK Library
+                    if (singleDetection.metadata != null) {
+                        // Check if the tag is the target
+                        if (aprilTagTargetID < 0 || singleDetection.id == aprilTagTargetID) {
+                            // If it's the target, lets get information from it.
+                            targetName    = singleDetection.metadata.name;
+                            targetID      = singleDetection.id;
+                            targetRange   = singleDetection.ftcPose.range;
+                            targetBearing = singleDetection.ftcPose.bearing;
+                            targetYaw     = singleDetection.ftcPose.yaw;
+                            targetFound   = true;
+                            break; // Take the first applicable tag, don't scan any others.
+                        } else {
+                            // This tag is in the library but is not a target.
+                            telemetry.addData("Skipping Tag", "Tag ID " + singleDetection.id + " is not desired");
+                            telemetry.update();
+                        }
+
+                    } else {
+                        telemetry.addData("Unknown Tag", "Tag is not in SDK Library");
+                        telemetry.update();
+                    }
+
+                } else {
+                    // TODO: Account for tag clusters.
+                    telemetry.addData("Wrong Tag Type", "Tag is not a SingleTag. It may be a cluster or other type.");
+                    telemetry.update();
+                }
+
+            }
+
+            if (targetFound) {
+                /*
+                Move the Servo.
+
+                Servo Values are 0-1, with .5 at the center.
+                targetBearing Values are -180 (180 degrees left) to 180 (180 degrees right), with 0 at the center.
+                */
+                double currentPos = rightServo.getPosition();
+                double targetPos  = 0.5 + (targetBearing / 180.0) * .5; // Translate the bearing to a servoPosition
+                double servoDelta = targetPos - currentPos;
+
+                // Set the new position of the servo, but ease it so it doesn't snap around abruptly.
+                rightServo.setPosition(currentPos + servoDelta * 0.1);
+
+                // Telemetry the AprilTag Data
+                telemetry.addData("Found", "ID %d (%s)", targetID, targetName);
+                telemetry.addData("Range",  "%5.1f inches", targetRange);
+                telemetry.addData("Bearing","%3.0f degrees", targetBearing);
+                telemetry.addData("Yaw","%3.0f degrees", targetYaw);
+            }
+
+            // Check only 50 times a second and give the CPU a break.
             sleep(20);
         }
 
-        // Save more CPU resources when camera is no longer needed.
+        // Save resources once the OpMode has ended.
         visionPortal.close();
 
-    }   // end method runOpMode()
+    }
 
     /**
      * Initialize the AprilTag processor.
@@ -133,11 +244,12 @@ public class AprilTagTest extends LinearOpMode {
                 BuiltinCameraDirection.BACK, aprilTag);
         }
 
-    }   // end method initAprilTag()
+    }
 
-    /**
-     * Add telemetry about AprilTag detections.
-     */
+    // The following code is commented out because I don't believe it's currently necessary.
+
+    /*
+    // Add telemetry about AprilTag detections.
     private void telemetryAprilTag() {
 
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
@@ -145,6 +257,7 @@ public class AprilTagTest extends LinearOpMode {
 
         // Step through the list of detections and display info for each one.
         for (AprilTagDetection detection : currentDetections) {
+
             if (detection instanceof AprilTagSingleDetection) {
                 AprilTagSingleDetection singleDet = (AprilTagSingleDetection) detection;
 
@@ -157,6 +270,7 @@ public class AprilTagTest extends LinearOpMode {
                     telemetry.addLine(String.format("\n==== (ID %d) Unknown", singleDet.id));
                     telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", singleDet.center.x, singleDet.center.y));
                 }
+
             }  else {
                 AprilTagClusterDetection clusterDet = (AprilTagClusterDetection) detection;
                 telemetry.addLine(String.format("\n==== Tag Cluster (%s)", clusterDet.metadata.name));
@@ -173,5 +287,5 @@ public class AprilTagTest extends LinearOpMode {
         telemetry.addLine("RBE = Range, Bearing & Elevation");
 
     }   // end method telemetryAprilTag()
-
+    */
 }   // end class
